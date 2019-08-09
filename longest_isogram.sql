b10c1ff90c8379bafb584f27a83f354a31f04fb4
Scott Schaen <MrSchaen@gmail.com>
2019-08-08 21:20:11 -0400

    Update longest_isogram.sql

--- longest_isogram.sql
+++ longest_isogram.sql
@@ -1,10 +1,13 @@
-WITH strings_of_words AS (
+-- Purpose: Find the longest isogram (words with only one of every letter).
+
+WITH words AS (
   --Put your string of words here. Make sure words are space separated
   SELECT 
-    lower(regexp_replace(title,r'[^A-Za-z]+',' ')) AS word_string, 
+    split(lower(regexp_replace(title,r'[^A-Za-z]+',' '))) AS word_string, 
     title AS original
   FROM `bigquery-samples.wikipedia_benchmark.Wiki10M` 
   WHERE language='en' AND lower(title) NOT LIKE '%file:%'
+  GROUP BY 1,2
   ), 
 words as (
   --Turn strings of words into one word per row
@@ -13,23 +16,12 @@ words as (
     word_string, 
     original
   FROM strings_of_words
-),
- isograms AS (
-  --Evaluate the letters per word
-  SELECT 
-    word, 
-    length(word) letters_in_word,
-    max((SELECT count(DISTINCT letters) FROM unnest(split(word,"")) as letters))   unique_letters_in_word
-    from words, unnest(word) word
-  GROUP BY 1,2
-  HAVING letters_in_word=unique_letters_in_word
 )
 SELECT 
-  isograms.letters_in_word, 
-  isograms.word, 
-  array_agg(struct(word_string, original) LIMIT 5) as source
-FROM isograms
-JOIN (SELECT word, word_string, original FROM words, unnest(word) word) words ON isograms.word=words.word
-WHERE letters_in_word>10
-GROUP BY 1,2
-ORDER BY letters_in_word DESC
+  word AS isogram, 
+  any_value((SELECT count(DISTINCT letters) FROM unnest(split(word,"")) as letters)) AS unique_letters_in_word,
+  array_agg(struct(original, word_string) LIMIT 5) AS source
+FROM words, unnest(word) word
+GROUP BY 1
+HAVING length(word)=unique_letters_in_word
+ORDER BY unique_letters_in_word DESC
