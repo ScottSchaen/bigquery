1eae1c263635d328b0ec6f3dfcf3475b9aa1ff03
Scott Schaen <MrSchaen@gmail.com>
2019-08-08 22:00:53 -0400

    Update longest_palindrome-multiple_words_per_row.sql

--- longest_palindrome-multiple_words_per_row.sql
+++ longest_palindrome-multiple_words_per_row.sql
@@ -6,6 +6,7 @@ WITH strings_of_words AS (
     title AS original
   FROM `bigquery-samples.wikipedia_benchmark.Wiki10M` 
   WHERE language='en' AND lower(title) NOT LIKE '%file:%'
+  GROUP BY 1,2
   ), 
 words as (
   -- Turn strings of words into one word per row
@@ -14,29 +15,17 @@ words as (
     word_string, 
     original
   FROM strings_of_words
-),
-palindromes as (
-  -- Find palindromes by comparing the word to the word reversed
-  SELECT 
-    word palindrome, 
-    length(word) number_of_letters, 
-    (SELECT struct(string_agg(letters,'' ORDER BY ordr DESC) as reverse_word, count(distinct letters) as unique_letters) FROM unnest(split(word,"")) letters WITH OFFSET ordr) x
-  FROM words, unnest(word) word
-  GROUP BY 1
-  HAVING palindrome = x.reverse_word
-  --Get rid of garbage palindromes with only 1 or 2 unique letters
-  AND x.unique_letters>2
-  ORDER BY number_of_letters DESC
-  LIMIT 1000
 )
--- Associate back to original string of words
 SELECT 
-  palindrome, 
-  number_of_letters,
+  word palindrome, 
+  length(word) number_of_letters, 
+  (SELECT count(distinct letters) FROM unnest(split(word,"")) letters) unique_letters,
   array_agg(struct(original, word_string) limit 3) as source
-FROM
-  palindromes
-JOIN
-  (SELECT word, word_string, original FROM words, unnest(word) word) words ON words.word = palindromes.palindrome
-GROUP BY 1,2 
+FROM words, unnest(word) word
+GROUP BY 1,2,3
+--Find palindromes by comparing word to word with reversed order
+HAVING word = (SELECT string_agg(letters,'' ORDER BY ordr DESC) FROM unnest(split(word,"")) letters WITH OFFSET ordr)
+--Get rid of garbage palindromes with only 1 or 2 unique letters
+AND unique_letters>2
 ORDER BY number_of_letters DESC
+LIMIT 1000
