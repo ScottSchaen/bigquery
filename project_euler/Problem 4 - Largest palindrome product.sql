76834ecce2bbf4a4bb22c306ab9e3b81debe879f
Scott Schaen <MrSchaen@gmail.com>
2019-08-24 11:19:12 -0400

    Update Problem 4 - Largest palindrome product.sql

--- project_euler/Problem 4 - Largest palindrome product.sql
+++ project_euler/Problem 4 - Largest palindrome product.sql
@@ -1,1 +1,10 @@
+-- A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+-- Find the largest palindrome made from the product of two 3-digit numbers.
 
+with nums as (select * from unnest(generate_array(100,999)) as num)
+select 
+  num1.num*num2.num prod
+from nums num1, nums num2
+where cast(num1.num*num2.num as string) = (SELECT string_agg(digits,'' ORDER BY ordr DESC) FROM unnest(split(cast(num1.num*num2.num AS STRING),"")) digits WITH OFFSET ordr)
+order by prod desc
+limit 1
